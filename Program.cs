31e96cf80050b46a116e59ae3b27b94e760a9db5
BakareHalimat <halimatbakare10@gmail.com>
2022-04-11 14:38:37 +0100

    week2assignment

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace week2class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+//variables in c#
+//these are comments they dont affect the code 
+
+//<data type> <name of variable> = <value
+
+
+
+           
+            Console.WriteLine("helloword!");
+            Console.WriteLine("fuhad");
+            Console.WriteLine("hello fuhad,i am in the backend track of wellaschools");
+            Console.WriteLine("hi i am learning back end because I feel it is the most important part of programming. Also it has a vital role in web application performance");
+            
+            
+            
+
+
+
+
+        }   
+
+    }
+}
+
+// variables inc#
