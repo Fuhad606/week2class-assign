168a4dfb434d6b3802e92c8524b096e2611216ea
BakareHalimat <halimatbakare10@gmail.com>
2022-04-11 15:53:40 +0100

    week2assignment

--- Program.cs
+++ Program.cs
@@ -11,15 +11,27 @@ namespace week2class
 //these are comments they dont affect the code 
 
 //<data type> <name of variable> = <value
-
-
+         string myname = "Fuhad";
+         int myage = 16;
+         float eurostodollars = 12;
+         long  abc = 12;
+         char charVar = 'A';
+         bool boolvar = true;
 
            
             Console.WriteLine("helloword!");
             Console.WriteLine("fuhad");
             Console.WriteLine("hello fuhad,i am in the backend track of wellaschools");
             Console.WriteLine("hi i am learning back end because I feel it is the most important part of programming. Also it has a vital role in web application performance");
+            Console.WriteLine(myname);
+            Console.WriteLine(myage);
+            Console.WriteLine(eurostodollars);
+            Console.WriteLine(abc);
+            Console.WriteLine(charVar);
+            Console.WriteLine(boolvar);
+
             
+
             
             
 
